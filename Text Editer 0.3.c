ff535f4e3727bec94eb97c4759125876a5597368
Ltabsyy <111294086+Ltabsyy@users.noreply.github.com>
2024-04-25 15:33:47 +0800

    Upgrade to version 0.3

--- Text Editer 0.3.c
+++ Text Editer 0.3.c
@@ -14,11 +14,13 @@ enum Color {
 	/*Color_Default = 0x07,
 	Color_Comment = 0x02,
 	Color_Symbol = 0x0c,
-	Color_Bracket = 0x0e,
+	Color_Bracket = 0x0c,//ced9
 	Color_Number = 0x0f,
 	Color_Preprocessor = 0x0d,
 	Color_Character = 0x0a,
 	Color_String = 0x06,
+	Color_ReservedWord = 0x05,
+	Color_Function = 0x0e,
 	Color_Variable = 0x0b*/
 	//低区分配色
 	Color_Default = 0x07,
@@ -29,7 +31,9 @@ enum Color {
 	Color_Preprocessor = 0x0d,
 	Color_Character = 0x09,
 	Color_String = 0x09,
-	Color_Variable = 0x07
+	Color_ReservedWord = 0x0d,
+	Color_Function = 0x0e,
+	Color_Variable = 0x0b
 };
 
 void ColorChar(char c, int color)//输出彩色字符
@@ -83,7 +87,8 @@ int FindMatch(int r, int c1, char ch)
 	{
 		if(content[r][c] == ch && color[r][c] == Color_Default)
 		{
-			if(ch == '\'' && content[r][c-1] == '\\')
+			if(ch == '\'' && content[r][c-1] == '\\'
+				&& c-2 >= 0 && content[r][c-2] != '\\')//判断'\''和'\\'
 			{
 				continue;
 			}
@@ -93,9 +98,43 @@ int FindMatch(int r, int c1, char ch)
 	return -1;
 }
 
+int FindReservedWord(int r1, int c1)
+{
+	static char* word[33] = {
+		"auto", "break", "case", "char", "const",
+		"continue", "default", "do", "double", "else",
+		"enum", "extern", "float", "for", "goto",
+		"if", "inline", "int", "long", "register",
+		"return", "short", "signed", "sizeof", "static",
+		"struct", "switch", "typedef", "union", "unsigned",
+		"void", "volatile", "while"
+	};
+	int i, j;
+	for(i=0; i<33; i++)//在所有关键字中比较
+	{
+		for(j=0; word[i][j]!=0; j++)
+		{
+			if(c1+j < numberOfColumn[r1] && content[r1][c1+j] == word[i][j]);
+			else break;
+		}
+		if(word[i][j] == 0)
+		{
+			if(c1+j < numberOfColumn[r1] && ((content[r1][c1+j] >= '0' && content[r1][c1+j] <= '9')
+				|| (content[r1][c1+j] >= 'A' && content[r1][c1+j] <= 'Z')
+				|| (content[r1][c1+j] >= 'a' && content[r1][c1+j] <= 'z')
+				|| content[r1][c1+j] == '_'))
+			{
+				continue;
+			}
+			return j;
+		}
+	}
+	return 0;//返回匹配关键字字符数
+}
+
 void AnalysisColor()
 {
-	int r, c, commentStart, characterStart, stringStart;
+	int r, c, commentStart/*, characterStart, stringStart*/;
 	int end;
 	color =(int**) calloc(numberOfRow, sizeof(int*));
 	//符号着色
@@ -300,8 +339,52 @@ void AnalysisColor()
 			}
 		}
 	}
+	//关键字着色
+	for(r=0; r<numberOfRow; r++)
+	{
+		for(c=0; c<numberOfColumn[r]; c++)
+		{
+			if(c > 0 && ((content[r][c-1] >= '0' && content[r][c-1] <= '9')
+				|| (content[r][c-1] >= 'A' && content[r][c-1] <= 'Z')
+				|| (content[r][c-1] >= 'a' && content[r][c-1] <= 'z')
+				|| content[r][c-1] == '_'));
+			else if(color[r][c] == Color_Default)//仅搜索未着色部分
+			{
+				end = FindReservedWord(r, c);
+				if(end > 0)
+				{
+					DrawColorInRange(r, c, r, c+end-1, Color_ReservedWord);
+				}
+			}
+		}
+	}
+	//函数着色
+	for(r=0; r<numberOfRow; r++)
+	{
+		for(c=0; c<numberOfColumn[r]-1; c++)
+		{
+			if(color[r][c] == Color_Default && content[r][c+1] == '(')
+			{
+				for(end=c; c>=0; c--)//(前为函数
+				{
+					if((content[r][c] >= '0' && content[r][c] <= '9')
+						|| (content[r][c] >= 'A' && content[r][c] <= 'Z')
+						|| (content[r][c] >= 'a' && content[r][c] <= 'z')
+						|| content[r][c] == '_')
+					{
+						color[r][c] = Color_Function;
+					}
+					else
+					{
+						break;
+					}
+				}
+				c = end;//防止死循环
+			}
+		}
+	}
 	//剩余内容均视为变量
-	/*for(r=0; r<numberOfRow; r++)
+	for(r=0; r<numberOfRow; r++)
 	{
 		for(c=0; c<numberOfColumn[r]; c++)
 		{
@@ -310,7 +393,7 @@ void AnalysisColor()
 				color[r][c] = Color_Variable;
 			}
 		}
-	}*/
+	}
 }
 
 void PrintContent()
@@ -322,8 +405,9 @@ void PrintContent()
 		{
 			if(content[r][c] == '\t')
 			{
-				printf("    ");
-				//printf("|   ");
+				//printf("    ");
+				ColorChar(':', Color_Bracket);
+				printf("   ");
 			}
 			/*else if((r+1)%4 == 0 && content[r][c] == ' ')
 			{
@@ -412,18 +496,55 @@ void ReadContent(char* fileName)
 	fclose(file);
 }
 
+void WriteContent(char* fileName)
+{
+	FILE* file = fopen(fileName, "w");
+	int r, c;
+	for(r=0; r<numberOfRow; r++)
+	{
+		for(c=0; c<numberOfColumn[r]; c++)
+		{
+			fputc(content[r][c], file);
+		}
+	}
+	fclose(file);
+}
+
 int main()
 {
 	char* fileName = InputFileName();
+	//char* fileName = "Text Editer.c";
+	int r;
 	ReadContent(fileName);
-	//ReadContent("Text Editer 0.2.c");
-	AnalysisColor();
 	system("chcp 65001");//以UTF-8编码显示
 	while(1)
 	{
+		AnalysisColor();
 		system("cls");
 		PrintContent();
 		getchar();
+		for(r=0; r<numberOfRow; r++)
+		{
+			free(color[r]);
+		}
+		free(color);
 	}
+	WriteContent(fileName);
 	return 0;
 }
+/*--------------------------------
+Text Editer 0.2
+——新增 数字小数点、十六进制数着色
+——优化 使用低区分配色
+——优化 UTF-8编码显示
+——优化 标识符内数字判断
+——优化 字符和字符串在'和"混杂场景着色
+——优化 不再将剩余内容均视为变量
+——修复 color内存分配错误
+——修复 多分配一行内存
+Text Editer 0.3
+——新增 关键字着色
+——新增 缩进提示线显示
+——新增 初步区分函数和变量
+——优化 更准确的判断'\''和'\\'
+--------------------------------*/
